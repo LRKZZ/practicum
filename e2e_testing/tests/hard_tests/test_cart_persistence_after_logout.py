92b178e2930cdb3a1dd593163343c446aff85d32
LRKZZ <sashulya1006@mail.ru>
2024-09-14 17:28:06 +0600

    fix: removed the definition of locators

--- e2e_testing/tests/hard_tests/test_cart_persistence_after_logout.py
+++ e2e_testing/tests/hard_tests/test_cart_persistence_after_logout.py
@@ -18,8 +18,8 @@ def test_cart_persistence_after_logout(page):
     cart_count = inventory_page.get_cart_count()
     assert cart_count == 2, f"Ожидалось 2 товара в корзине, но их {cart_count}"
 
-    page.click("button#react-burger-menu-btn")
-    page.click("a#logout_sidebar_link")
+    inventory_page.page.click("button#react-burger-menu-btn")
+    inventory_page.page.click("a#logout_sidebar_link")
 
     assert page.url == "https://www.saucedemo.com/", "Ожидался переход на страницу логина после выхода"
 
@@ -31,7 +31,6 @@ def test_cart_persistence_after_logout(page):
     print(f"Товары в корзине после повторного входа: {cart_items}")
 
     assert any("Sauce Labs Backpack" in item for item in cart_items), "Товар 'Sauce Labs Backpack' не найден в корзине после повторного входа"
-    
     assert any("Sauce Labs Bike Light" in item for item in cart_items), "Товар 'Sauce Labs Bike Light' не найден в корзине после повторного входа"
 
     assert len(cart_items) == 2, f"Ожидалось, что в корзине будет 2 товара после повторного входа, но их {len(cart_items)}"
