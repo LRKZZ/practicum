9865c03eca0ff043c8c8a99411b06305b775029b
LRKZZ <sashulya1006@mail.ru>
2024-09-14 16:57:44 +0600

    fix: migrate locators from methods to constructor

--- e2e_testing/pages/inventory_po.py
+++ e2e_testing/pages/inventory_po.py
@@ -1,28 +1,35 @@
 class InventoryPage:
     def __init__(self, page):
         self.page = page
+        self.add_to_cart_button_prefix = "button[id='add-to-cart-"
+        self.cart_link = "a.shopping_cart_link"
+        self.sort_container = "select.product_sort_container"
+        self.item_price_locator = ".inventory_item_price"
+        self.add_all_buttons = "button.btn_inventory"
+        self.cart_badge = ".shopping_cart_badge"
+        self.inventory_item = ".inventory_item"
 
     def add_to_cart(self, item_id):
-        self.page.click(f"button[id='add-to-cart-{item_id}']")
+        self.page.click(f"{self.add_to_cart_button_prefix}{item_id}']")
 
     def open_cart(self):
-        self.page.click("a.shopping_cart_link")
+        self.page.click(self.cart_link)
 
     def sort_by(self, sort_type):
-        self.page.select_option("select.product_sort_container", sort_type)
+        self.page.select_option(self.sort_container, sort_type)
 
     def get_item_prices(self):
-        prices = self.page.locator(".inventory_item_price").all_text_contents()
+        prices = self.page.locator(self.item_price_locator).all_text_contents()
         return [float(price.replace('$', '')) for price in prices]
 
     def add_all_items_to_cart(self):
-        item_buttons = self.page.locator("button.btn_inventory").all()
+        item_buttons = self.page.locator(self.add_all_buttons).all()
         for button in item_buttons:
             button.click()
 
     def get_cart_count(self):
-        cart_count_text = self.page.locator(".shopping_cart_badge").text_content()
+        cart_count_text = self.page.locator(self.cart_badge).text_content()
         return int(cart_count_text) if cart_count_text else 0
 
     def get_total_item_count(self):
-        return len(self.page.locator(".inventory_item").all())
+        return len(self.page.locator(self.inventory_item).all())
--- e2e_testing/pages/login_po.py
+++ e2e_testing/pages/login_po.py
@@ -1,8 +1,12 @@
 class LoginPage:
     def __init__(self, page):
         self.page = page
+        # Локаторы
+        self.username_input = "input#user-name"
+        self.password_input = "input#password"
+        self.login_button = "input#login-button"
 
     def login(self, username, password):
-        self.page.fill("input#user-name", username)
-        self.page.fill("input#password", password)
-        self.page.click("input#login-button")
+        self.page.fill(self.username_input, username)
+        self.page.fill(self.password_input, password)
+        self.page.click(self.login_button)
